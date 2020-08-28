6fcdfc61c86989e5465551bd5661ef62fdf32b69
mugukamil <mugukamil@gmail.com>
2020-08-28 20:45:43 +0300

    wip

--- sunburst.js
+++ sunburst.js
@@ -12,7 +12,7 @@ const drawDiagram = (valuefield) => {
             .padRadius(radius * 1.5)
             .innerRadius(d => d.y0 * radius)
             .outerRadius(d => Math.max(d.y0 * radius, d.y1 * radius - 1));
-  
+
     const partition = data => {
         const root = d3.hierarchy(data)
                 .sum(d => d[valueField])
@@ -46,22 +46,24 @@ const drawDiagram = (valuefield) => {
         }
      ]
     };
-  
+
     const root = partition(data);
     const color = d3.scaleOrdinal([`#387c85`, `#f29ebe`]);
-  
-  
+
+
     root.each(d => d.current = d);
-  
+
     const svg = d3.select('#partitionSVG').append('svg')
             .attr("id", "svg-component")
             .style("width", "100%")
             .style("height", "700px")
             .style("font", "10px sans-serif");
-  
+
     const g = svg.append("g")
             .attr("transform", `translate(${width / 2},${width / 2})`);
-  
+
+            console.log(root.descendants().filter(d => !d.parent)[0]);
+
     const path = g.append("g")
             .selectAll("path")
             .data(root.descendants().slice(1))
@@ -71,33 +73,55 @@ const drawDiagram = (valuefield) => {
                     d = d.parent;
                 return color(d.data.name);
             })
-            .attr("fill-opacity", d => arcVisible(d.current) ? (d.children ? 0.6 : 0.4) : 0)
-            .attr("d", d => arc(d.current));
-  
+            .attr("fill-opacity", d => arcVisible(d.current) ? (d.children ? 1 : 0) : 0)
+            .attr("d", d => arc(d.current))
+            // .classed('hidden', function(d) {
+            //   if (!d.children) {
+            //     return true;
+            //   }
+            // })
+            // .style("display", function(d) {
+            //     if (d.depth > 1) {
+            //       return "none";//nodes whose depth is more than 1 make its vanish
+            //     } else {
+            //       return "";
+            //     }
+            //   });
+
+    // path.filter(d => !d.children).style('display', 'none')
     path.filter(d => d.children)
             .style("cursor", "pointer")
             .on("click", clicked);
-  
-  
+
+
     path.on("mouseover", function(d) {
               tooltip.transition()
                   .duration(200)
                   .style("opacity", 1);
               tooltip.html(
-              `</p>${d.ancestors().map(d => d.data.name).reverse().join("/")}\n${format(d.value)}<p>`
+              `<p>${d.data.name}</p>`
                   )
-                  .style("left", to(d3.event.pageX - 10) + "px")
+                  .style("left", (d3.event.pageX - 10) + "px")
                   .style("top", (d3.event.pageY) + "px")
+                  if (tooltip.classed('dedicated') && d.data.classname !== 'dedicated') {
+                    tooltip.classed('dedicated', false);
+                    tooltip.classed('mainstreaming', true);
+                  } else if (tooltip.classed('mainstreaming') && d.data.classname !== 'mainstreaming') {
+                    tooltip.classed('mainstreaming', false);
+                    tooltip.classed('dedicated', true);
+                  } else {
+                    tooltip.classed(d.data.classname, true);
+                  }
               })
-              
+
           .on("mouseout", function(d) {
               tooltip.transition()
                   .duration(500)
                   .style("opacity", 0);
           });
-  
-      
-  
+
+
+
     const label = g.append("g")
             .attr("pointer-events", "none")
             .attr("text-anchor", "middle")
@@ -106,32 +130,45 @@ const drawDiagram = (valuefield) => {
             .data(root.descendants().slice(1))
             .join("text")
             .attr("class", function(d){
-              return d.data.classname; 
+              return d.data.classname;
               })
             .attr("dy", "0.35em")
             .attr("fill-opacity", d => +labelVisible(d.current))
             .attr("transform", d => labelTransform(d.current))
-            .text(d => d.value);
-  
+            .text(d => d.value)
+            // .classed('hidden', function(d) {
+            //   if (!d.children) {
+            //     return true;
+            //   }
+            // })
+            // .style("display", function(d) {
+            //     console.log(d);
+            //     if (d.depth > 1) {
+            //       return "none";//nodes whose depth is more than 1 make its vanish
+            //     } else {
+            //       return "";
+            //     }
+            //   });
+
     const parent = g.append("circle")
             .datum(root)
             .attr("r", radius)
             .attr("fill", "none")
             .attr("pointer-events", "all")
             .on("click", clicked);
-  
+
     function clicked(p) {
         parent.datum(p.parent || root);
-  
+
         root.each(d => d.target = {
                 x0: Math.max(0, Math.min(1, (d.x0 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
                 x1: Math.max(0, Math.min(1, (d.x1 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
                 y0: Math.max(0, d.y0 - p.depth),
                 y1: Math.max(0, d.y1 - p.depth)
-            });
-  
+            })
+
         const t = g.transition().duration(750);
-  
+
         // Transition the data on all arcs, even the ones that aren’t visible,
         // so that if this transition is interrupted, entering arcs will start
         // the next transition from the desired position.
@@ -145,32 +182,31 @@ const drawDiagram = (valuefield) => {
                 })
                 .attr("fill-opacity", d => arcVisible(d.target) ? (d.children ? 0.6 : 0.4) : 0)
                 .attrTween("d", d => () => arc(d.current));
-  
+
         label.filter(function (d) {
             return +this.getAttribute("fill-opacity") || labelVisible(d.target);
         }).transition(t)
                 .attr("fill-opacity", d => +labelVisible(d.target))
                 .attrTween("transform", d => () => labelTransform(d.current));
     }
-  
+
     function arcVisible(d) {
         return d.y1 <= 3 && d.y0 >= 1 && d.x1 > d.x0;
     }
-  
+
     function labelVisible(d) {
         return d.y1 <= 3 && d.y0 >= 1 && (d.y1 - d.y0) * (d.x1 - d.x0) > 0.03;
     }
-  
+
     function labelTransform(d) {
         const x = (d.x0 + d.x1) / 2 * 180 / Math.PI;
         const y = (d.y0 + d.y1) / 2 * radius;
         return `rotate(${x - 90}) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
     }
-  
+
   }
-  
+
   const onValueFieldChange = (value) => {
      drawDiagram(value);
   }
   drawDiagram('lower');
-  
