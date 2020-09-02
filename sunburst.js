ff19597dac108e9cf08cd01ef207924e9afc07dc
mugukamil <mugukamil@gmail.com>
2020-09-03 00:16:48 +0300

    make 1st donut, add reset button

--- sunburst.js
+++ sunburst.js
@@ -1,17 +1,18 @@
-const drawDiagram = (valueField, data, cb) => {
+loadData();
+
+function loadData() {
+  d3.json("data.json").then((data) => {
+    drawDiagram("lower", data);
+  });
+}
+
+function drawDiagram(valueField, data, cb) {
   d3.select("#svg-component").remove();
   const tooltip = d3.select(".tooltip").style("opacity", 0);
   const format = d3.format(",d");
   const width = 600;
-  const radius = width / 6;
-  const arc = d3
-    .arc()
-    .startAngle((d) => d.x0)
-    .endAngle((d) => d.x1)
-    .padAngle((d) => Math.min((d.x1 - d.x0) / 2, 0.005))
-    .padRadius(radius * 1.5)
-    .innerRadius((d) => d.y0 * radius)
-    .outerRadius((d) => Math.max(d.y0 * radius, d.y1 * radius - 1));
+  const radius = width / 1.5;
+  const ANIMATION_TIMEOUT = 5000;
 
   const hierarchy = d3
     .hierarchy(data)
@@ -20,12 +21,33 @@ const drawDiagram = (valueField, data, cb) => {
     })
     .sort((a, b) => b.value - a.value);
 
-  const partition = d3.partition().size([2 * Math.PI, hierarchy.height + 1]);
+  const partition = d3.partition().size([2 * Math.PI, radius]);
+
+  const arc = d3
+    .arc()
+    .startAngle((d) => d.x0)
+    .endAngle((d) => d.x1)
+    .padAngle(0.005)
+    .padRadius(radius / 1.5)
+    .innerRadius(function (d) {
+      if (d.depth == 1) {
+        return radius * 1.2 - d.y1;
+      }
+      return radius - d.y1;
+    })
+    .outerRadius(function (d) {
+      if (d.depth == 1) {
+        return radius - d.y0 + 10;
+      }
+      return radius * 1.2 - d.y0;
+    });
 
   const root = partition(hierarchy);
   const color = d3.scaleOrdinal(["#387c85", "#f29ebe"]);
 
-  root.each((d) => (d.current = d));
+  root.each((d) => {
+    d.current = d;
+  });
 
   const svg = d3
     .select("#partitionSVG")
@@ -46,15 +68,13 @@ const drawDiagram = (valueField, data, cb) => {
     .attr("fill", (d) => {
       while (d.depth > 1) d = d.parent;
       return color(d.data.name);
-    })
-    .attr("fill-opacity", (d) => (arcVisible(d) ? (d.children ? 1 : 0) : 0));
+    });
 
   path
+    .filter((d) => !d.children)
     .transition()
     .delay(function (d, i) {
-      while (i < 2) {
-        return i * 500;
-      }
+      return i * 500;
     })
     .duration(500)
     .attrTween("d", function (d) {
@@ -64,55 +84,16 @@ const drawDiagram = (valueField, data, cb) => {
         return arc(d);
       };
     });
-  // .attr("d", (d) => arc(d));
 
-  path
-    .filter((d) => d.children)
-    .style("cursor", "pointer")
-    .on("click", clicked);
+  path.style("cursor", "pointer");
+
+  let showTotalTimeout = setTimeout(showTotal, ANIMATION_TIMEOUT);
+  let showTotalTimeoutCallCount = 0;
 
   path
     .on("mouseover", function (d) {
-      tooltip
-        .filter((d) => +this.getAttribute("fill-opacity"))
-        .transition()
-        .duration(200)
-        .style("opacity", 1);
-      tooltip
-        .filter((d) => +this.getAttribute("fill-opacity"))
-        .html(`<p>${d.data.name}</p>`)
-        .style("left", () => {
-          let left = arc.centroid(d)[0];
-          if (d.target) {
-            left = arc.centroid(d.target)[0];
-          }
-          if (Math.sign(left) == -1) {
-            // left -= x;
-          } else {
-            // left += x;
-          }
-          if (d.depth < 2) {
-            return left + 300 + "px";
-          } else {
-            return left + 300 + "px";
-          }
-        })
-        .style("top", () => {
-          let top = arc.centroid(d)[1];
-          if (d.target) {
-            top = arc.centroid(d.target)[1];
-          }
-          if (Math.sign(top) == -1) {
-            // top -= y;
-          } else {
-            // top += y;
-          }
-          if (d.depth < 2) {
-            return top + 300 + "px";
-          } else {
-            return top + 300 + "px";
-          }
-        });
+      tooltip.transition().duration(200).style("opacity", 1);
+      tooltip.html(`<p>${d.data.name}</p>`);
       if (tooltip.classed("dedicated") && d.data.classname !== "dedicated") {
         tooltip.classed("dedicated", false);
         tooltip.classed("mainstreaming", true);
@@ -126,7 +107,20 @@ const drawDiagram = (valueField, data, cb) => {
         tooltip.classed(d.data.classname, true);
       }
     })
+    .on("mousemove", function (d) {
+      tooltip
+        .style("left", () => {
+          // let left = arc.centroid(d)[0];
+          return `${d3.event.layerX + 10}px`;
+        })
+        .style("top", () => {
+          // let top = arc.centroid(d)[1];
+          return `${d3.event.layerY + 10}px`;
+        });
+      clearTimeout(showTotalTimeout);
+    })
     .on("mouseout", function (d) {
+      showTotalTimeout = setTimeout(showTotal, ANIMATION_TIMEOUT);
       tooltip.transition().duration(500).style("opacity", 0);
     });
 
@@ -142,142 +136,63 @@ const drawDiagram = (valueField, data, cb) => {
       return d.data.classname;
     })
     .attr("dy", "0.35em")
-    .attr("fill-opacity", (d) => (+labelVisible(d) ? (d.children ? 1 : 0) : 0))
-    .attr("transform", (d) => labelTransform(d))
+    .attr("transform", (d) => labelTransform(d));
+
+  label
+    .filter((d) => !d.children)
+    .transition()
+    .delay(function (d, i) {
+      return i * 500;
+    })
+    .duration(500)
     .text((d) => d.value);
-
-  const parent = g
-    .append("circle")
-    .datum(root)
-    .attr("r", radius)
-    .attr("fill", "none")
-    .attr("pointer-events", "all")
-    .on("click", clicked);
-
-  function clicked(p) {
-    parent.datum(p.parent || root);
-
-    root.each((d) => {
-      d.target = {
-        x0:
-          Math.max(0, Math.min(1, (d.x0 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
-        x1:
-          Math.max(0, Math.min(1, (d.x1 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
-        y0: Math.max(0, d.y0 - p.depth),
-        y1: Math.max(0, d.y1 - p.depth),
-      };
-      d.clicked = p;
-
-      return d;
-    });
-
-    const t = g.transition().duration(750);
-
-    // Transition the data on all arcs, even the ones that aren’t visible,
-    // so that if this transition is interrupted, entering arcs will start
-    // the next transition from the desired position.
-    path
-      .transition(t)
-      .tween("data", (d) => {
-        const i = d3.interpolate(d.current, d.target);
-        return (t) => (d.current = i(t));
-      })
-      .filter(function (d) {
-        return +this.getAttribute("fill-opacity") || arcVisible(d.target);
-      })
-      .attr("fill-opacity", (d) =>
-        arcVisible(d.target) ? (d.children ? 1 : 0.8) : 0
-      )
-      .attrTween("d", (d) => () => arc(d.current));
-
-    label
-      .filter(function (d) {
-        return +this.getAttribute("fill-opacity") || labelVisible(d.target);
-      })
-      .transition(t)
-      .attr("fill-opacity", (d) => +labelVisible(d.target))
-      .attrTween("transform", (d) => () => labelTransform(d.current));
-  }
 
   d3.selectAll("input[name='valuefield']").on("click", function () {
     root.sum((d) => d[this.value]);
     partition(root);
 
-    let isTarget = null;
+    path.transition().duration(750).attr("d", arc);
 
-    root.each((d) => {
-      if (d.clicked) {
-        isTarget = d.target;
-        d.target = {
-          x0:
-            Math.max(
-              0,
-              Math.min(1, (d.x0 - d.clicked.x0) / (d.clicked.x1 - d.clicked.x0))
-            ) *
-            2 *
-            Math.PI,
-          x1:
-            Math.max(
-              0,
-              Math.min(1, (d.x1 - d.clicked.x0) / (d.clicked.x1 - d.clicked.x0))
-            ) *
-            2 *
-            Math.PI,
-          y0: Math.max(0, d.y0 - d.clicked.depth),
-          y1: Math.max(0, d.y1 - d.clicked.depth),
-        };
-      }
-      return d;
-    });
+    label
+      .transition()
+      .duration(750)
+      .text((d) => d.value)
+      .attr("transform", (d) => labelTransform(d.current));
+  });
 
-    if (!isTarget) {
+  function showTotal() {
+    if (!showTotalTimeoutCallCount) {
       path
+        .filter((d) => d.children)
         .transition()
-        .duration(750)
-        .attr("d", (d) => arc(d.current));
-
-      label
-        .transition()
-        .duration(750)
-        .text((d) => d.value)
-        .attr("transform", (d) => labelTransform(d.current));
-    } else {
-      path
-        .transition()
-        .duration(750)
-        .tween("data", (d) => {
-          const i = d3.interpolate(d.current, d.target);
-          return (t) => (d.current = i(t));
+        .delay(function (d, i) {
+          return i * 500;
         })
-        .attrTween("d", (d) => () => {
-          return arc(d.current);
+        .duration(500)
+        .attrTween("d", function (d) {
+          var i = d3.interpolate(d.x0, d.x1);
+          return function (t) {
+            d.x1 = i(t);
+            return arc(d);
+          };
         });
 
       label
+        .filter((d) => d.children)
         .transition()
-        .duration(750)
-        .text((d) => {
-          return d.value;
+        .delay(function (d, i) {
+          return i * 500;
         })
-        .attrTween("transform", (d) => () => labelTransform(d.current));
+        .duration(500)
+        .text((d) => d.value);
+
+      showTotalTimeoutCallCount++;
     }
-  });
-
-  function arcVisible(d) {
-    return d.y1 <= 2 && d.y0 >= 1 && d.x1 > d.x0;
-  }
-
-  function labelVisible(d) {
-    return d.y1 <= 2 && d.y0 >= 1 && (d.y1 - d.y0) * (d.x1 - d.x0) > 0.03;
   }
 
   function labelTransform(d) {
     const x = (((d.x0 + d.x1) / 2) * 180) / Math.PI;
-    const y = ((d.y0 + d.y1) / 2) * radius;
+    const y = radius - (d.y0 + d.y1) / 2.5;
     return `rotate(${x - 90}) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
   }
-};
-
-d3.json("data.json").then((data) => {
-  drawDiagram("lower", data);
-});
+}
